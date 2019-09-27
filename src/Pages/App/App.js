e4eea16211b9d99108550462aae223fa85d3c5c7
Chris <cwill833@gmail.com>
2019-09-26 22:43:37 -0400

    organize file structure

---
+++ src/Pages/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component{
+
+  render(){
+
+    return(
+      <div className="main">
+        <h1>Hello I will be your website</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
